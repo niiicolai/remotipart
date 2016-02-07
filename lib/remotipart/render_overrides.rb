16239cf516f023128cf8d94e00dbe7adf47a49ea
Nicolai Berg Andersen <niiicolai@gmail.com>
2016-02-07 03:23:12 +0100

    x

--- lib/remotipart/render_overrides.rb
+++ lib/remotipart/render_overrides.rb
@@ -7,28 +7,17 @@ module Remotipart
     def self.included(base)
       base.class_eval do
         alias_method_chain :render, :remotipart
-        alias_method_chain :head, :remotipart
       end
     end
 
-    def render_with_remotipart(*args)
-      render_without_remotipart(*args).tap do
-        patch_remotipart
-      end
-    end
-
-    def head_with_remotipart(*args)
-      head_without_remotipart(*args).tap do
-        patch_remotipart
-      end
-    end
-
-    private
-      def patch_remotipart
-        return if !remotipart_submitted?
+    def render_with_remotipart *args
+      render_without_remotipart *args
+      if remotipart_submitted?
         textarea_body = response.content_type == 'text/html' ? html_escape(response.body) : response.body
-        response.body = %{<textarea data-type=\"#{response.content_type}\" data-status=\"#{response.response_code}\" data-statusText=\"#{response.message}\">#{textarea_body}</textarea>}
+        response.body = %{<script type=\"text/javascript\">try{window.parent.document;}catch(err){document.domain=document.domain;}</script>#{textarea_body}}
         response.content_type = Mime::HTML
       end
+      response_body
+    end
   end
 end
